115a3d52eb111c0ab42fea1351925a8431314c15
Jack <6464072@qq.com>
2025-09-26 16:46:04 +0800

    img

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'youke1.picui.cn',
+        port: '',
+        pathname: '/s1/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
