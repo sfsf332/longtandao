62b5108973d9f3993d71e3becfac62da07b7d56c
Jack <6464072@qq.com>
2025-10-17 11:35:37 +0800

    fix:photo path error

--- src/app/events/chess/s1/components/GallerySection.js
+++ src/app/events/chess/s1/components/GallerySection.js
@@ -233,142 +233,142 @@ export default function GallerySection() {
           {/* 对弈照片 */}
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/duiyi0.jpg" alt="对弈现场1" width={380} height={280} />
+              <Image src="/photo/duiyi0.jpg" alt="对弈现场1" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>激烈的对弈现场</div>
           </div>
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/duiyi1.jpg" alt="对弈现场2" width={380} height={280} />
+              <Image src="/photo/duiyi1.jpg" alt="对弈现场2" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>选手专注思考</div>
           </div>
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/duiyi2.jpg" alt="对弈现场3" width={380} height={280} />
+              <Image src="/photo/duiyi2.jpg" alt="对弈现场3" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>棋局精彩对决</div>
           </div>
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/duiyi3.jpg" alt="对弈现场4" width={380} height={280} />
+              <Image src="/photo/duiyi3.jpg" alt="对弈现场4" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>高手过招瞬间</div>
           </div>
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/duiyi4.jpg" alt="对弈现场5" width={380} height={280} />
+              <Image src="/photo/duiyi4.jpg" alt="对弈现场5" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>深度思考时刻</div>
           </div>
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/duiyi5.jpg" alt="对弈现场6" width={380} height={280} />
+              <Image src="/photo/duiyi5.jpg" alt="对弈现场6" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>棋手风采展示</div>
           </div>
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/duiyi6.jpg" alt="对弈现场7" width={380} height={280} />
+              <Image src="/photo/duiyi6.jpg" alt="对弈现场7" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>紧张对弈氛围</div>
           </div>
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/duiyi7.jpg" alt="对弈现场8" width={380} height={280} />
+              <Image src="/photo/duiyi7.jpg" alt="对弈现场8" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>智慧的较量</div>
           </div>
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/duiyi8.jpg" alt="对弈现场9" width={380} height={280} />
+              <Image src="/photo/duiyi8.jpg" alt="对弈现场9" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>精彩对局收官</div>
           </div>
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/duiyi9.jpg" alt="对弈现场9" width={380} height={280} />
+              <Image src="/photo/duiyi9.jpg" alt="对弈现场9" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>精彩瞬间记录</div>
           </div>
           {/* 领奖照片 */}
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/lingjiang1.jpg" alt="颁奖典礼" width={380} height={280} />
+              <Image src="/photo/lingjiang1.jpg" alt="颁奖典礼" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>来打酱油也光荣</div>
           </div>
           {/* 围观照片 */}
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/weiguan0.jpg" alt="观众围观1" width={380} height={280} />
+              <Image src="/photo/weiguan0.jpg" alt="观众围观1" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>观众热情围观</div>
           </div>
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/weiguan1.jpg" alt="观众围观2" width={380} height={280} />
+              <Image src="/photo/weiguan1.jpg" alt="观众围观2" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>现场气氛热烈</div>
           </div>
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/weiguan2.jpg" alt="观众围观3" width={380} height={280} />
+              <Image src="/photo/weiguan2.jpg" alt="观众围观3" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>观众专注观赛</div>
           </div>
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/weiguan3.jpg" alt="观众围观4" width={380} height={280} />
+              <Image src="/photo/weiguan3.jpg" alt="观众围观4" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>围观群众助威</div>
           </div>
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/weiguan4.jpg" alt="观众围观5" width={380} height={280} />
+              <Image src="/photo/weiguan4.jpg" alt="观众围观5" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>观赛人群聚集</div>
           </div>
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/weiguan5.jpg" alt="观众围观6" width={380} height={280} />
+              <Image src="/photo/weiguan5.jpg" alt="观众围观6" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>现场观众互动</div>
           </div>
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/weiguan6.jpg" alt="观众围观7" width={380} height={280} />
+              <Image src="/photo/weiguan6.jpg" alt="观众围观7" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>观众津津有味</div>
           </div>
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/weiguan7.jpg" alt="观众围观8" width={380} height={280} />
+              <Image src="/photo/weiguan7.jpg" alt="观众围观8" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>围观学习交流</div>
           </div>
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/weiguan8.jpg" alt="观众围观9" width={380} height={280} />
+              <Image src="/photo/weiguan8.jpg" alt="观众围观9" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>棋手专注对弈</div>
           </div>
           {/* 现场照片 */}
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/xianchang1.jpg" alt="比赛现场1" width={380} height={280} />
+              <Image src="/photo/xianchang1.jpg" alt="比赛现场1" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>比赛现场全景</div>
           </div>
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/xianchang2.jpg" alt="比赛现场2" width={380} height={280} />
+              <Image src="/photo/xianchang2.jpg" alt="比赛现场2" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>赛事组织有序</div>
           </div>
           <div className={styles.galleryItem}>
             <div className={styles.galleryImage}>
-              <Image src="/Photo/xianchang3.jpg" alt="比赛现场3" width={380} height={280} />
+              <Image src="/photo/xianchang3.jpg" alt="比赛现场3" width={380} height={280} />
             </div>
             <div className={styles.galleryCaption}>现场布置整齐</div>
           </div>
