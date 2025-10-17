2ecc53fb89ec87ce231f4dd3d71a90de3f115cc7
Jack <6464072@qq.com>
2025-10-17 12:59:03 +0800

    debug

--- src/app/events/chess/s1/components/MobileCarousel.js
+++ src/app/events/chess/s1/components/MobileCarousel.js
@@ -1,7 +1,6 @@
 'use client';
 
 import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import Image from 'next/image';
 import styles from '../page.module.css';
 
